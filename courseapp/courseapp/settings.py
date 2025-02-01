1177e205f1c63e6a2d1b2f4b29447331afc9b1f7
Nhất Duy <nhatduy242@gmail.com>
2025-02-01 17:17:47 +0700

    video 1 10:00

--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'courses.User'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- courseapp/courses/models.py
+++ courseapp/courses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+      pass
+
+
+class Category(models.Model):
+      name = models.CharField(max_length = 50, null = False)
+      
+      def __str__(self):
+            return self.name
