27a6763a3a868017fb6a3c07cc02b9e1073de8bd
Nhất Duy <nhatduy242@gmail.com>
2025-02-02 14:46:49 +0700

    hết video 1

--- courseapp/courses/models.py
+++ courseapp/courses/models.py
@@ -7,8 +7,27 @@ class User(AbstractUser):
       pass
 
 
-class Category(models.Model):
+class BaseModel(models.Model):
+      created_date = models.DateField(auto_now_add=True, null=True)
+      updated_date = models.DateTimeField(auto_now=True, null=True)
+      active = models.BooleanField(default=True)
+      
+      class Meta:
+            abstract = True
+
+
+class Category(BaseModel):
       name = models.CharField(max_length = 50, null = False)
       
       def __str__(self):
             return self.name
+
+
+class Course(BaseModel):
+      subject = models.CharField(max_length=255, null = False)
+      description = models.TextField()
+      img = models.CharField(max_length=100)
+      Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+      
+      def __str__(self):
+            return self.subject
